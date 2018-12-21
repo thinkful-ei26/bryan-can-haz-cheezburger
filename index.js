9b69242d7be8bd263d4cc2eba809577294b1e694
Fantosism <businessspamfolder@gmail.com>
2018-12-21 09:34:07 -0700

    :construction: basic api get endpoint

--- index.js
+++ index.js
@@ -1,41 +1,64 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
+const { PORT, CLIENT_ORIGIN } = require('./config')
+const { dbConnect } = require('./db-mongoose')
 // const {dbConnect} = require('./db-knex');
 
-const app = express();
+const app = express()
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
-  })
-);
+    skip: (req, res) => process.env.NODE_ENV === 'test',
+  }),
+)
 
 app.use(
   cors({
-    origin: CLIENT_ORIGIN
-  })
-);
+    origin: CLIENT_ORIGIN,
+  }),
+)
+
+app.get('/api/cheeses', (err, res) => {
+  res.status(200)
+  res.json([
+    'Bath Blue',
+    'Barkham Blue',
+    'Buxton Blue',
+    'Cheshire Blue',
+    'Devon Blue',
+    'Dorset Blue Vinney',
+    'Dovedale',
+    'Exmoor Blue',
+    'Harbourne Blue',
+    'Lanark Blue',
+    'Lymeswold',
+    'Oxford Blue',
+    'Shropshire Blue',
+    'Stichelton',
+    'Stilton',
+    'Blue Wensleydale',
+    'Yorkshire Blue',
+  ])
+})
 
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
+      console.info(`App listening on port ${server.address().port}`)
     })
     .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+      console.error('Express failed to start')
+      console.error(err)
+    })
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+  dbConnect()
+  runServer()
 }
 
-module.exports = { app };
+module.exports = { app }
